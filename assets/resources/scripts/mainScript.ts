b92758a73fddcdc4d4c034beb83364e12463cd47
Aryan Khanna <aryan.khanna@chicmic.co.in>
2023-06-29 17:57:26 +0530

    singleton Class updated

--- assets/resources/scripts/mainScript.ts
+++ assets/resources/scripts/mainScript.ts
@@ -26,66 +26,22 @@ export class mainScript extends Component {
   alphaNode: Node = null;
   @property({ type: Node })
   bannerNode: Node = null;
-  soundFolder: AudioClip[] = [];
 
   onLoad() {
+    singleton.getInstance().AlphabetButton = this.alphabhetButton;
+    singleton.getInstance().AlphabetNode = this.alphaNode;
+    singleton.getInstance().AlphabetPositionJSON = this.alphabhetPositions;
     singleton.getInstance().BackgroundNodeHolder = this.node;
     singleton.getInstance().BannerNode = this.bannerNode;
-    this.loadSprites()
-      .then(() => this.loadAudios())
-      .then(() => this.addButtons())
+    singleton
+      .getInstance()
+      .loadSprites()
+      .then(() => singleton.getInstance().loadAudios())
+      .then(() => singleton.getInstance().addButtons())
       .catch((err) => console.error(err));
   }
   protected start(): void {
     this.bannerNode.getComponent(Animation).play("entry");
-  }
-  loadSprites() {
-    return new Promise<void>((resolve, reject) => {
-      resources.loadDir("sprites/prefab/alphabhets", SpriteFrame, (err, assets) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        singleton.getInstance().ExampleImages = assets;
-        console.log(assets);
-        resolve();
-      });
-    });
-  }
-
-  loadAudios() {
-    return new Promise<void>((resolve, reject) => {
-      resources.loadDir("sounds/alphabhetSounds", AudioClip, (err, assets) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-
-        this.soundFolder = assets;
-        console.log(this.soundFolder);
-        resolve();
-      });
-    });
-  }
-
-  addButtons() {
-    let audioClipIndex = 0;
-    for (let i = 1; i < Object.keys(this.alphabhetPositions.json[0].posData).length + 1; i++) {
-      let rowNumber = `row${i}`;
-      console.log(rowNumber);
-
-      for (let key in this.alphabhetPositions.json[0].posData[rowNumber]) {
-        let alphaButton = instantiate(this.alphabhetButton);
-        alphaButton.name = `${key}`;
-        alphaButton.getComponent(AudioSource).clip = this.soundFolder[audioClipIndex];
-        audioClipIndex++;
-
-        let pos = this.alphabhetPositions.json[0].posData[rowNumber][key];
-        alphaButton.setPosition(new Vec3(pos.x, pos.y, 0));
-
-        this.alphaNode.addChild(alphaButton);
-      }
-    }
   }
 
   update(deltaTime: number) {}
--- assets/resources/scripts/singleton.ts
+++ assets/resources/scripts/singleton.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, SpriteFrame } from "cc";
+import { _decorator, AudioClip, AudioSource, Component, instantiate, JsonAsset, Node, Prefab, resources, SpriteFrame, Vec3 } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("singleton")
@@ -8,6 +8,11 @@ export class singleton extends Component {
   private exampleImages: SpriteFrame[] = [];
   private nodeHolder: Node = null;
   private bannerNode: Node = null;
+  private alphabetNode: Node = null;
+
+  private alphabetButton: Prefab = null;
+  private alphabetPositionJSON: JsonAsset = null;
+  soundFolder: AudioClip[] = [];
   private singleton() {}
 
   static getInstance(): singleton {
@@ -21,6 +26,12 @@ export class singleton extends Component {
   }
   set Target(value: string) {
     this.target = value;
+  }
+  get AlphabetPositionJSON(): JsonAsset {
+    return this.alphabetPositionJSON;
+  }
+  set AlphabetPositionJSON(value: JsonAsset) {
+    this.alphabetPositionJSON = value;
   }
 
   get ExampleImages(): SpriteFrame[] {
@@ -41,7 +52,67 @@ export class singleton extends Component {
   set BannerNode(value: Node) {
     this.bannerNode = value;
   }
+  get AlphabetButton(): Prefab {
+    return this.alphabetButton;
+  }
+  set AlphabetButton(value: Prefab) {
+    this.alphabetButton = value;
+  }
+  get AlphabetNode(): Node {
+    return this.alphabetNode;
+  }
+  set AlphabetNode(value: Node) {
+    this.alphabetNode = value;
+  }
   start() {}
+  loadSprites() {
+    return new Promise<void>((resolve, reject) => {
+      resources.loadDir("sprites/prefab/alphabhets", SpriteFrame, (err, assets) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        singleton.getInstance().ExampleImages = assets;
+        console.log(assets);
+        resolve();
+      });
+    });
+  }
+
+  loadAudios() {
+    return new Promise<void>((resolve, reject) => {
+      resources.loadDir("sounds/alphabhetSounds", AudioClip, (err, assets) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        this.soundFolder = assets;
+        console.log(this.soundFolder);
+        resolve();
+      });
+    });
+  }
+
+  addButtons() {
+    let audioClipIndex = 0;
+    for (let i = 1; i < Object.keys(this.alphabetPositionJSON.json[0].posData).length + 1; i++) {
+      let rowNumber = `row${i}`;
+      console.log(rowNumber);
+
+      for (let key in this.alphabetPositionJSON.json[0].posData[rowNumber]) {
+        let alphaButton = instantiate(this.alphabetButton);
+        alphaButton.name = `${key}`;
+        alphaButton.getComponent(AudioSource).clip = this.soundFolder[audioClipIndex];
+        audioClipIndex++;
+
+        let pos = this.alphabetPositionJSON.json[0].posData[rowNumber][key];
+        alphaButton.setPosition(new Vec3(pos.x, pos.y, 0));
+
+        this.alphabetNode.addChild(alphaButton);
+      }
+    }
+  }
 
   update(deltaTime: number) {}
 }
